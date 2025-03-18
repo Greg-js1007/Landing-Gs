7da5841d9f5653e5a011483abbcb0640c84ee304
Gregory Subero <suberog010@gmail.com>
2025-03-18 11:49:21 -0400

    Agregando el html al javascript y la logica

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const url = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC3aj05GEEyzdOqYM5FLSFeg&part=snippet%2Cid&order=date&maxResults=5';
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC3aj05GEEyzdOqYM5FLSFeg&part=snippet%2Cid&order=date&maxResults=5';
+
+const content = null || document.getElementById('content')
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -13,3 +16,26 @@ async function fetchData(urlApi){
     return data; 
 }
 
+(async () => {
+    try{
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+            <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.tumbnail.high.url}" alt="${video.snippet.description}" class="w-full" />
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${video.snippet.title}
+                </h3>
+            </div>
+        </div>
+        `).slice(0,4).join('')}`;
+        content.innerHTML = view; 
+    }
+    catch{
+
+    }
+})();
